7189466fb97f88c7d38c667076d3cd9a9f1f89a5
Johnny Chen <mcjohnnychen@gmail.com>
2022-04-10 03:41:25 -0700

    fix colemak layer

--- keyboards/sofle/keymaps/mcjcc/keymap.c
+++ keyboards/sofle/keymaps/mcjcc/keymap.c
@@ -171,7 +171,7 @@ KC_LSFT, KC_Z  , KC_X   , KC_C   , KC_V   , KC_B   , KC_VOLD,       KC_PGDN, KC_
   KC_ESC , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_VOLU,       KC_PGUP,  KC_J    , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSPC,
   KC_TAB , KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_MUTE,       KC_NO  ,  KC_M    , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,
   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_VOLD,       KC_PGDN,  KC_K    , KC_H   , KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-                   KC_LCTRL, KC_LALT ,KC_LGUI,TG(_LOWER), KC_ENT ,       KC_ENT , TG(_RAISE),KC_SPC, KC_RALT, KC_RCTRL
+                   KC_LCTRL, KC_LALT ,KC_LGUI,MO(_LOWER), KC_ENT ,       KC_ENT , MO(_RAISE),KC_SPC, KC_RALT, KC_RCTRL
 ),
 
 /* LOWER
